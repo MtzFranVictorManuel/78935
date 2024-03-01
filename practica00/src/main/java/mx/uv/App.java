5972a69aa549c3bb599e36919a68727277994ce3
MtzFranVictorManuel <zs22024137@estudiantes.uv.mx>
2024-03-01 16:52:40 -0600

    practica00

---
+++ practica00/src/main/java/mx/uv/App.java
@@ -0,0 +1,17 @@
+package mx.uv;
+
+import static spark.Spark.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        get("/xml", (request, response) -> {
+            response.type("application/xml");
+            return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><saludo>Hola Mundo</saludo>";
+        });
+    }
+}
