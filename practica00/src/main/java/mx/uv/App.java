c9438591e83e1f67d2b5c9b3fc9849f42f4c2b08
MtzFranVictorManuel <zs22024137@estudiantes.uv.mx>
2024-03-08 15:10:33 -0600

    practica00

--- practica00/src/main/java/mx/uv/App.java
+++ practica00/src/main/java/mx/uv/App.java
@@ -1,6 +1,9 @@
 package mx.uv;
 
 import static spark.Spark.*;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
 /**
  * Hello world!
  *
@@ -9,7 +12,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        //CORS comentario
+        System.out.println( "Hello World!" );
+
+        // para permitir el CORS
         //fuente:https://gist.github.com/saeidzebardast/e375b7d17be3e0f4dddf
         options("/*",(request,response)->{
         String accessControlRequestHeaders=request.headers("Access-Control-Request-Headers");
@@ -24,9 +29,68 @@ public class App
         });
         before((request,response)->response.header("Access-Control-Allow-Origin","*"));
 
-        get("/xml", (request, response) -> {
+
+        // (pr1, pr2) -> "";
+        // (pr1, pr2) -> {return null;};
+        // refactorización
+        get("/", (request, response)-> "hola");
+
+        get("/xml", (request, response)-> {
+            String respuesta=
+            "<p>" + 
+                "<x>" + 
+                    "<y>algo</y>" + 
+                "</x>" + 
+                "<x><y>algo2</y></x>" + 
+            "</p>";
             response.type("application/xml");
-            return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><saludo>Hola Mundo</saludo>";
+            return respuesta;
+        });
+
+        get("/xml2", (request, response)-> {
+            InnerApp whatsapp = new InnerApp();
+            whatsapp.setNombreApp("whatsapp");
+            whatsapp.setIdApp("1234");
+            whatsapp.setComments("aplicación de mensajería");
+
+            /*
+            String respuesta = "<nombre>" +
+                whatsapp.getNombreApp() +
+                "</nombre>";
+                */
+
+            XmlMapper xml = new XmlMapper();
+            String respuesta = xml.writeValueAsString(whatsapp);
+            response.type("application/xml");
+            return respuesta;
         });
     }
+}
+
+/**
+ * InnerApp
+ */
+class InnerApp {
+    String nombreApp;
+    String idApp;
+    String comments;
+
+    public String getNombreApp() {
+        return nombreApp;
+    }
+    public void setNombreApp(String nombreApp) {
+        this.nombreApp = nombreApp;
+    }
+    public String getIdApp() {
+        return idApp;
+    }
+    public void setIdApp(String idApp) {
+        this.idApp = idApp;
+    }
+    public String getComments() {
+        return comments;
+    }
+    public void setComments(String comments) {
+        this.comments = comments;
+    }    
 }
