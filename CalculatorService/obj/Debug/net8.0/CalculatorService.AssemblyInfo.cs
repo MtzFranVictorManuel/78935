236846e4ac86586a1e3cb276af1cbf70ba5f8597
MtzFranVictorManuel <zs22024137@estudiantes.uv.mx>
2024-05-09 16:22:17 -0600

    practica05

--- CalculatorService/obj/Debug/net8.0/CalculatorService.AssemblyInfo.cs
+++ CalculatorService/obj/Debug/net8.0/CalculatorService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0681176f7fab93a2fa514b9ec0212aec339283b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+051e6b366782243840421a42805cbeb0090f7072")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- practica02/practica02/obj/Debug/net8.0/practica02.AssemblyInfo.cs
+++ practica02/practica02/obj/Debug/net8.0/practica02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practica02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0681176f7fab93a2fa514b9ec0212aec339283b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+051e6b366782243840421a42805cbeb0090f7072")]
 [assembly: System.Reflection.AssemblyProductAttribute("practica02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practica02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
